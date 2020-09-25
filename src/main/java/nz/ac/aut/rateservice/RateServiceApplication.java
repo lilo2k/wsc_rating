dda460876a471c73403b83af1d88284e8d752009
Tommy Yoon <tdw9746@autuni.ac.nz>
2020-09-26 00:56:04 +1200

    initial commits

---
+++ src/main/java/nz/ac/aut/rateservice/RateServiceApplication.java
@@ -0,0 +1,26 @@
+package nz.ac.aut.rateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class RateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateServiceApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String rate() {
+	return String.format("list");
+	}  
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "Web Services") String name) {
+	return String.format("Hello %s!", name);
+	}  
+}
---
+++ src/test/java/nz/ac/aut/rateservice/RateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nz.ac.aut.rateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
