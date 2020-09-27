760be927be15c4f25962010a9ddc7e8fdcaddbb3
Tommy Yoon <tdw9746@autuni.ac.nz>
2020-09-27 20:35:36 +1300

    Exception handling

--- src/main/java/nz/ac/aut/rateservice/controller/RateRestController.java
+++ src/main/java/nz/ac/aut/rateservice/controller/RateRestController.java
@@ -4,16 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 
 import nz.ac.aut.rateservice.dto.RateDTO;
+import nz.ac.aut.rateservice.exception.ExceptionResponse;
+import nz.ac.aut.rateservice.exception.RateNotFoundException;
 import nz.ac.aut.rateservice.model.Rate;
 import nz.ac.aut.rateservice.service.RateService;
 import nz.ac.aut.rateservice.util.ObjectMapperUtils;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/")
+// @RequestMapping("/")
 public class RateRestController {
 
     @Autowired
@@ -21,25 +26,40 @@ public class RateRestController {
 
     @GetMapping(value = "/")
     public List<RateDTO> getAllRates() {
-        return ObjectMapperUtils.mapAll(rateService.findAll(), RateDTO.class);
-    }
+        List<Rate> rates = rateService.findAll();
 
-    @GetMapping(value = "/{jobID}")
-    public RateDTO getRateByJobID(@PathVariable("jobID") String jobID) {
-        return ObjectMapperUtils.map(rateService.findByJobID(jobID), RateDTO.class);
+        if (rates.size() == 0)
+            throw new RateNotFoundException("No data found");
+
+        return ObjectMapperUtils.mapAll(rates, RateDTO.class);
     }
 
     @GetMapping(value = "/orderByJobID")
     public List<RateDTO> findAllByOrderByJobID() {
-        return ObjectMapperUtils.mapAll(rateService.findAllByOrderByJobID(), RateDTO.class);
+        List<Rate> rates = rateService.findAllByOrderByJobID();
+
+        if (rates == null)
+            throw new RateNotFoundException("No data found");
+
+        return ObjectMapperUtils.mapAll(rates, RateDTO.class);
+    }
+
+    @GetMapping(value = "/{jobID}")
+    public RateDTO getRateByJobID(@PathVariable("jobID") String jobID) {
+        Rate rate = rateService.findByJobID(jobID);
+
+        if (rate == null)
+            throw new RateNotFoundException("Job ID: " + jobID);
+
+        return ObjectMapperUtils.map(rate, RateDTO.class);
     }
 
     // @PostMapping(value = "/")
     // public ResponseEntity<?> saveOrUpdate(@RequestBody RateDTO rateDTO) {
-    //     rateService.saveOrUpdate(ObjectMapperUtils.map(rateDTO, Rate.class));
-    //     return new ResponseEntity("Rate added successfully", HttpStatus.OK);
+    // rateService.saveOrUpdate(ObjectMapperUtils.map(rateDTO, Rate.class));
+    // return new ResponseEntity("Rate added successfully", HttpStatus.OK);
     // }
-    
+
     @PostMapping(value = "/{jobID}/{rate}")
     public ResponseEntity<?> createRate(@PathVariable("jobID") String jobID, @PathVariable("rate") int rate) {
         rateService.saveOrUpdate(ObjectMapperUtils.map(new RateDTO(jobID, rate), Rate.class));
@@ -49,6 +69,10 @@ public class RateRestController {
     @PutMapping(value = "/{jobID}/{rate}")
     public ResponseEntity<?> modify(@PathVariable("jobID") String jobID, @PathVariable("rate") int rate) {
         Rate rateObject = rateService.findByJobID(jobID);
+
+        if (rateObject == null)
+            return ResponseEntity.notFound().build();
+
         rateObject.setRate(rate);
         rateService.saveOrUpdate(rateObject);
         return new ResponseEntity("Rate updated successfully", HttpStatus.OK);
@@ -56,7 +80,12 @@ public class RateRestController {
 
     @DeleteMapping(value = "/{jobID}")
     public ResponseEntity<?> deleteRateByJobID(@PathVariable("jobID") String jobID) {
-        rateService.deleteById(rateService.findByJobID(jobID).getId());
+        Rate rateObject = rateService.findByJobID(jobID);
+
+        if (rateObject == null)
+            return ResponseEntity.notFound().build();
+
+        rateService.deleteById(rateObject.getId());
         return new ResponseEntity("Rate deleted successfully", HttpStatus.OK);
     }
 }
